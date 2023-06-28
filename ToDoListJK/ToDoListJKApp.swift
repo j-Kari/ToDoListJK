b6c2dec24c6b108f63fabe67d4eff519267f38d8
Kiran K <rogrdat@outlook.com>
2023-06-28 10:50:44 -0400

    Initial Commit

---
+++ ToDoListJK/ToDoListJKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListJKApp.swift
+//  ToDoListJK
+//
+//  Created by Kiran Kari on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListJKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
